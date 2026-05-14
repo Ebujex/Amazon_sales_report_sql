4f1c79b9c3812b17fc294437f19f3ed256ebd778
Joshua Ejelonu <ebujex01@gmail.com>
2026-05-14 12:41:58 +0100

    Update database_create.sql

--- scripts/database_create.sql
+++ scripts/database_create.sql
@@ -1,1 +1,19 @@
+/*
+creating database and schemas
+*/
 
+USE Master
+GO
+---create 'AmazonSales' database
+CREATE DATABASE AmazonSales;
+GO
+  
+USE AmazonSales;
+GO
+---create Schemas
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
